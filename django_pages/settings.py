829c391f8c11cc73a957e6667cc4edb3a50863a3
ConnorTregre <70208148+ConnorTregre@users.noreply.github.com>
2020-11-03 12:55:21 -0600

    update

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p)w%$wh^rev$hc0k8z1g_s&s&u8rhw(hqvvw#!o19b$&j8ss@i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
